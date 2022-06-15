c849ec024ba2973e94bd9382d25f20a874d58ab8
archanavelusamy7 <archanavelusamy@yahoo.com>
2022-06-15 22:52:22 +0530

    First Commit

---
+++ src/main/java/com/springboot/management/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.springboot.management.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.springboot.management.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+	
+
+}
+//List<Employee> findAllEmployee();
---
+++ src/main/java/com/springboot/management/service/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.springboot.management.service;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import com.springboot.management.model.Employee;
+
+public interface EmployeeService {
+	List<Employee> getAllEmployees();
+	void saveEmployee(Employee employee);
+	Employee getEmployeeById(long id);
+	void deleteEmployeeById(long id);
+	Page<Employee> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
+}
